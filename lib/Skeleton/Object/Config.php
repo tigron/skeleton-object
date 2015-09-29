2dc5005fbb2340f9fd502edbafab489c2881bbd8
David Vandemaele <david@tigron.be>
2015-09-29 14:58:09 +0200

    Add config setting to allow auto update of slug

---
+++ lib/Skeleton/Object/Config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Config class
+ * Configuration for Skeleton\Object
+ *
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+namespace Skeleton\Object;
+
+class Config {
+
+	/**
+	 * Auto update the slug
+	 *
+	 * This flag will be used to allow auto updating the slug
+	 *
+	 * @access public
+	 * @var bool $auto_update_slug
+	 */
+	public static $auto_update_slug = false;
+
+}
