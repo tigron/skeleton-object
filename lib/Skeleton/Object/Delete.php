1f4a76aa7475e44e8f328a254cd660a4f0a34dea
Christophe Gosiau <christophe.gosiau@tigron.be>
2015-11-19 17:06:27 +0100

    Stop using 'soft_delete'
    $class_configuration['soft_delete'] is deprecated in favor of 'delete()', 'archive()' and 'restore()'

--- lib/Skeleton/Object/Delete.php
+++ lib/Skeleton/Object/Delete.php
@@ -10,12 +10,21 @@
 namespace Skeleton\Object;
 
 trait Delete {
+
 	/**
 	 * Delete
 	 *
 	 * @access public
 	 */
 	public function delete() {
+		/**
+		 * This code is to prevent unwanted deletion
+		 * It is temporary and should be removed in later versions
+		 */
+		if (property_exists(get_class(), 'class_configuration') AND isset(self::$class_configuration['soft_delete']) AND self::$class_configuration['soft_delete'] === TRUE) {
+			throw new \Exception('It seems you don\'t want to delete this object, please use \"archive()\" instead');
+		}
+
 		$table = self::trait_get_database_table();
 		$db = self::trait_get_database();
 
@@ -26,20 +35,26 @@ trait Delete {
 			}
 		}
 
-		if (property_exists(get_class(), 'class_configuration') AND isset(self::$class_configuration['soft_delete']) AND self::$class_configuration['soft_delete'] === TRUE) {
-			$this->deleted = date('YmdHis');
-			$this->save();
-		} else {
-			$db->query('DELETE FROM ' . $table . ' WHERE ' . self::trait_get_table_field_id() . '=?', [$this->id]);
-		}
+		$db->query('DELETE FROM ' . $table . ' WHERE ' . self::trait_get_table_field_id() . '=?', [$this->id]);
 	}
 
+	/**
+	 * Archive
+	 *
+	 * @access public
+	 */
+	public function archive() {
+		$this->archived = date('Y-m-d H:i:s');
+		$this->save();
+	}
+
+	/**
+	 * Restore
+	 *
+	 * @access public
+	 */
 	public function restore() {
-		if (property_exists(get_class(), 'class_configuration') AND isset(self::$class_configuration['soft_delete']) AND self::$class_configuration['soft_delete'] === TRUE) {
-			$this->deleted = '0000-00-00 00:00:00';
-			$this->save();
-		} else {
-			throw new \Exception('This object cannot be restored');
-		}
+		$this->archived = '0000-00-00 00:00:00';
+		$this->save();
 	}
 }
