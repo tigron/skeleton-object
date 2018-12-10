12792ed919e8ae602309cd8b62c6dc5df09f8ba9
David Vandemaele <david@tigron.be>
2018-12-10 16:10:26 +0100

    Add memcached option TCP_NODELY

--- lib/Skeleton/Object/Cache/Handler/Memcached.php
+++ lib/Skeleton/Object/Cache/Handler/Memcached.php
@@ -77,7 +77,7 @@ class Memcached implements \Skeleton\Object\Cache\HandlerInterface {
 			self::$memcached->setOption(\Memcached::OPT_SERIALIZER, \Memcached::SERIALIZER_IGBINARY);
 			self::$memcached->setOption(\Memcached::OPT_BINARY_PROTOCOL, true);
 			self::$memcached->setOption(\Memcached::OPT_NO_BLOCK, true);
-			self::$memcached->setOption(\Memcached::OPT_BINARY_PROTOCOL, true);
+			self::$memcached->setOption(\Memcached::OPT_TCP_NODELAY, true);
 			self::$memcached->addServers([
 				[$config['hostname'], $config['port']]
 			]);
